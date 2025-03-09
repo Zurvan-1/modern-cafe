6836729135f9c5215254a50ad5e58577ded9a006
araz <araz.maher.irani@gmail.com>
2025-03-09 20:40:12 +0330

    Initial Cafe Project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Hero from './assets/components/Hero/Hero'
+import Services from './assets/components/Services/Services'
+import WhereToBuy from './assets/components/WhereToBuy/WhereToBuy'
+import AppBanner from './assets/components/AppBanner/AppBanner'
+import Footer from './assets/components/Footer/Footer'
+
+export default function App() {
+  return (
+    <div className='overflow-x-hidden'>
+      <Hero />
+      <Services />
+      <WhereToBuy />
+      <AppBanner />
+      <Footer />
+    </div>
+  )
+}
